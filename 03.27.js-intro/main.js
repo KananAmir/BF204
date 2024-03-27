189f138e2900717b957995b558ec4b653a139424
KananAmir <amirovknn@gmail.com>
2024-03-27 21:43:52 +0400

    js intro

---
+++ 03.27.js-intro/main.js
@@ -0,0 +1,22 @@
+// let, const, var
+// var firstName
+
+var userName = "Emin";
+
+// console.log(userName);
+
+var userName = "lorem";
+
+console.log(userName);
+
+let count = 0;
+
+count--;
+
+count = count - 1;
+
+//falsy values
+
+// 0, -0, false, "", ``, NaN, null, undefined;
+
+1 && 2 && null && 3 
